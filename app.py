eeeab3d598720d3f4bdd13f9fe6ac0b6dd9fc5b6
Teifion Jordan <sarkalian@gmail.com>
2019-09-28 15:10:03 +0100

    Updated app to behave as expected by the test, URL no longer echoed back

--- app.py
+++ app.py
@@ -7,12 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   swatch_result = swatcher.swatch_url(request.args['url'])
-
-  response = {
-    "url": request.args['url'],
-    "swatch": swatch_result
-  }
-  return jsonify(response)
+  return swatch_result
 
 if __name__ == '__main__':
   app.run()
